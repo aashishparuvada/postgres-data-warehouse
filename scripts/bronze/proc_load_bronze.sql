2880d9e354fea943757113f601386e961336f450
aashishparuvada <aashishvardhanp@gmail.com>
2026-03-04 14:02:59 +0530

    loading-bronze

---
+++ scripts/bronze/proc_load_bronze.sql
@@ -0,0 +1,150 @@
+/*
+===============================================================================
+Stored Procedure: Load Bronze Layer (Source -> Bronze)
+===============================================================================
+Loads CSV data into bronze tables.
+===============================================================================
+
+Use your machine specific path at FROM while you load your datasets.
+Also make sure you have the files on the same host machine/server.
+*/
+
+CREATE OR REPLACE PROCEDURE bronze.load_bronze()
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    start_time TIMESTAMP;
+    end_time TIMESTAMP;
+    batch_start_time TIMESTAMP;
+    batch_end_time TIMESTAMP;
+BEGIN
+
+    batch_start_time := CURRENT_TIMESTAMP;
+
+    RAISE NOTICE '================================================';
+    RAISE NOTICE 'Loading Bronze Layer';
+    RAISE NOTICE '================================================';
+
+    RAISE NOTICE '------------------------------------------------';
+    RAISE NOTICE 'Loading CRM Tables';
+    RAISE NOTICE '------------------------------------------------';
+
+    start_time := CURRENT_TIMESTAMP;
+
+    RAISE NOTICE '>> Truncating Table: bronze.crm_cust_info';
+    TRUNCATE TABLE bronze.crm_cust_info;
+
+    RAISE NOTICE '>> Inserting Data Into: bronze.crm_cust_info';
+    COPY bronze.crm_cust_info
+    FROM '/sql/dwh_project/datasets/source_crm/cust_info.csv'
+    WITH (FORMAT csv, HEADER true);
+
+    end_time := CURRENT_TIMESTAMP;
+
+    RAISE NOTICE '>> Load Duration: % seconds',
+        EXTRACT(EPOCH FROM (end_time - start_time));
+
+    RAISE NOTICE '>> -------------';
+
+
+    start_time := CURRENT_TIMESTAMP;
+
+    RAISE NOTICE '>> Truncating Table: bronze.crm_prd_info';
+    TRUNCATE TABLE bronze.crm_prd_info;
+
+    RAISE NOTICE '>> Inserting Data Into: bronze.crm_prd_info';
+    COPY bronze.crm_prd_info
+    FROM '/sql/dwh_project/datasets/source_crm/prd_info.csv'
+    WITH (FORMAT csv, HEADER true);
+
+    end_time := CURRENT_TIMESTAMP;
+
+    RAISE NOTICE '>> Load Duration: % seconds',
+        EXTRACT(EPOCH FROM (end_time - start_time));
+
+    RAISE NOTICE '>> -------------';
+
+
+    start_time := CURRENT_TIMESTAMP;
+
+    RAISE NOTICE '>> Truncating Table: bronze.crm_sales_details';
+    TRUNCATE TABLE bronze.crm_sales_details;
+
+    RAISE NOTICE '>> Inserting Data Into: bronze.crm_sales_details';
+    COPY bronze.crm_sales_details
+    FROM '/sql/dwh_project/datasets/source_crm/sales_details.csv'
+    WITH (FORMAT csv, HEADER true);
+
+    end_time := CURRENT_TIMESTAMP;
+
+    RAISE NOTICE '>> Load Duration: % seconds',
+        EXTRACT(EPOCH FROM (end_time - start_time));
+
+    RAISE NOTICE '>> -------------';
+
+
+    RAISE NOTICE '------------------------------------------------';
+    RAISE NOTICE 'Loading ERP Tables';
+    RAISE NOTICE '------------------------------------------------';
+
+
+    start_time := CURRENT_TIMESTAMP;
+
+    TRUNCATE TABLE bronze.erp_loc_a101;
+
+    COPY bronze.erp_loc_a101
+    FROM '/sql/dwh_project/datasets/source_erp/loc_a101.csv'
+    WITH (FORMAT csv, HEADER true);
+
+    end_time := CURRENT_TIMESTAMP;
+
+    RAISE NOTICE '>> Load Duration: % seconds',
+        EXTRACT(EPOCH FROM (end_time - start_time));
+
+
+    start_time := CURRENT_TIMESTAMP;
+
+    TRUNCATE TABLE bronze.erp_cust_az12;
+
+    COPY bronze.erp_cust_az12
+    FROM '/sql/dwh_project/datasets/source_erp/cust_az12.csv'
+    WITH (FORMAT csv, HEADER true);
+
+    end_time := CURRENT_TIMESTAMP;
+
+    RAISE NOTICE '>> Load Duration: % seconds',
+        EXTRACT(EPOCH FROM (end_time - start_time));
+
+
+    start_time := CURRENT_TIMESTAMP;
+
+    TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+
+    COPY bronze.erp_px_cat_g1v2
+    FROM '/sql/dwh_project/datasets/source_erp/px_cat_g1v2.csv'
+    WITH (FORMAT csv, HEADER true);
+
+    end_time := CURRENT_TIMESTAMP;
+
+    RAISE NOTICE '>> Load Duration: % seconds',
+        EXTRACT(EPOCH FROM (end_time - start_time));
+
+
+    batch_end_time := CURRENT_TIMESTAMP;
+
+    RAISE NOTICE '==========================================';
+    RAISE NOTICE 'Loading Bronze Layer is Completed';
+    RAISE NOTICE 'Total Load Duration: % seconds',
+        EXTRACT(EPOCH FROM (batch_end_time - batch_start_time));
+    RAISE NOTICE '==========================================';
+
+
+EXCEPTION
+WHEN OTHERS THEN
+    RAISE NOTICE '==========================================';
+    RAISE NOTICE 'ERROR OCCURRED DURING LOADING BRONZE LAYER';
+    RAISE NOTICE 'Error Message: %', SQLERRM;
+    RAISE NOTICE '==========================================';
+
+END;
+$$;
